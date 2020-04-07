3a87d2768b0d2d0200e3ad583ca90f5ddbe49962
Haritha Sulekha <63001939+HarithaSulekha@users.noreply.github.com>
2020-04-07 22:40:05 +0530

    Add files via upload

---
+++ DeepNeuralNetwork.py
@@ -0,0 +1,123 @@
+import numpy as np
+import h5py
+import matplotlib.pyplot as plt
+from dnn_utils_v2 import sigmoid, sigmoid_backward, relu, relu_backward
+
+plt.rcParams['figure.figsize'] = (5.0, 4.0) # set default size of plots
+plt.rcParams['image.interpolation'] = 'nearest'
+plt.rcParams['image.cmap'] = 'gray'
+np.random.seed(1)
+
+# GRADED FUNCTION: initialize_parameters
+
+def initialize_parameters_deep(layer_dims):
+    np.random.seed(3)
+    parameters = {}
+    L = len(layer_dims)
+    for l in range(1, L):
+        parameters['W' + str(l)] = np.random.randn(layer_dims[l], layer_dims[l - 1]) * 0.01
+        parameters['b' + str(l)] = np.zeros((layer_dims[l], 1))
+        assert (parameters['W' + str(l)].shape == (layer_dims[l], layer_dims[l - 1]))
+        assert (parameters['b' + str(l)].shape == (layer_dims[l], 1))
+
+    return parameters
+
+# GRADED FUNCTION: linear_forward
+def linear_forward(A, W, b):
+    Z = np.dot(W, A) + b
+    assert (Z.shape == (W.shape[0], A.shape[1]))
+    cache = (A, W, b)
+    return Z, cache
+
+# GRADED FUNCTION: linear_activation_forward
+def linear_activation_forward(A_prev, W, b, activation):
+    if activation == "sigmoid":
+        Z, linear_cache = linear_forward(A_prev, W, b)
+        A, activation_cache = sigmoid(Z)
+
+    elif activation == "relu":
+        Z, linear_cache = linear_forward(A_prev, W, b)
+        A, activation_cache = relu(Z)
+
+    assert (A.shape == (W.shape[0], A_prev.shape[1]))
+    cache = (linear_cache, activation_cache)
+    return A, cache
+
+# GRADED FUNCTION: L_model_forward
+def L_model_forward(X, parameters):
+    caches = []
+    A = X
+    L = len(parameters) // 2
+    for l in range(1, L):
+        A_prev = A
+        A, cache = linear_activation_forward(A_prev, parameters['W' + str(l)], parameters['b' + str(l)],activation="relu")
+        caches.append(cache)
+        AL, cache = linear_activation_forward(A, parameters['W' + str(L)], parameters['b' + str(L)],activation="sigmoid")
+        caches.append(cache)
+        assert (AL.shape == (1, X.shape[1]))
+        return AL, caches
+
+# GRADED FUNCTION: compute_cost
+def compute_cost(AL, Y):
+    m = Y.shape[1]
+    cost = -1 / m * (np.dot(Y, np.log(AL.T)) + np.dot(1 - Y, np.log(1 - AL).T))
+    cost = np.squeeze(cost)   # To make sure your cost's shape is what we expect (e.g. this turns [[17]] into 17).
+    assert (cost.shape == ())
+    return cost
+
+# GRADED FUNCTION: linear_backward
+def linear_backward(dZ, cache):
+    A_prev, W, b = cache
+    m = A_prev.shape[1]
+    dW = 1 / m * (np.dot(dZ, A_prev.T))
+    db = 1 / m * (np.sum(dZ, axis=1, keepdims=True))
+    dA_prev = np.dot(W.T, dZ)
+    assert (dA_prev.shape == A_prev.shape)
+    assert (dW.shape == W.shape)
+    assert (db.shape == b.shape)
+    return dA_prev, dW, db
+
+# GRADED FUNCTION: linear_activation_backward
+def linear_activation_backward(dA, cache, activation):
+    linear_cache, activation_cache = cache
+
+    if activation == "relu":
+        dZ = relu_backward(dA, activation_cache)
+        dA_prev, dW, db = linear_backward(dZ, linear_cache)
+
+    elif activation == "sigmoid":
+        dZ = sigmoid_backward(dA, activation_cache)
+        dA_prev, dW, db = linear_backward(dZ, linear_cache)
+
+    return dA_prev, dW, db
+
+# GRADED FUNCTION: L_model_backward
+def L_model_backward(AL, Y, caches):
+    grads = {}
+    L = len(caches)
+    m = AL.shape[1]
+    Y = Y.reshape(AL.shape)
+    dAL = - (np.divide(Y, AL) - np.divide(1 - Y, 1 - AL))
+    current_cache = caches[L - 1]
+    grads["dA" + str(L - 1)], grads["dW" + str(L)], grads["db" + str(L)] = linear_activation_backward(dAL,current_cache,activation="sigmoid")
+    for l in reversed(range(L - 1)):
+        current_cache = caches[l]
+        dA_prev_temp, dW_temp, db_temp = linear_activation_backward(grads["dA" + str(l + 1)], current_cache,activation="relu")
+        grads["dA" + str(l)] = dA_prev_temp
+        grads["dW" + str(l + 1)] = dW_temp
+        grads["db" + str(l + 1)] = db_temp
+    return grads
+
+# GRADED FUNCTION: update_parameters
+def update_parameters(parameters, grads, learning_rate):
+    L = len(parameters) // 2
+    for l in range(L):
+        parameters["W" + str(l + 1)] = parameters["W" + str(l + 1)] - learning_rate * grads["dW" + str(l + 1)]
+        parameters["b" + str(l + 1)] = parameters["b" + str(l + 1)] - learning_rate * grads["db" + str(l + 1)]
+    return parameters
+
+
+
+
+
+
